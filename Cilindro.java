4ef84425b10f3bd2227c8181692547b26c0a49e1
Joaquín Gonzalo Paredes Mescco <91225726+joaquinGPM@users.noreply.github.com>
2023-05-31 13:36:29 -0500

    Update Cilindro.java

--- Cilindro.java
+++ Cilindro.java
@@ -1,1 +1,13 @@
-
+public class Cilindro{
+    private int alt;
+    public Cilindro(int alt){
+          this.alt= alt;
+  }
+    public int getalt() {
+          return alt;
+    }
+ 
+    public void setalt(int alt) {
+          this.alt = alt
+    }
+}
